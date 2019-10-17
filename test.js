5dbc6d18ecd9a5a930f73b428fe31a16e0bb5b96
Raviraajan-lab <56407742+Raviraajan-lab@users.noreply.github.com>
2019-10-17 17:42:07 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+});
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', event => {
+  button.innerHTML = `Click count: ${event.detail}`;
+});
